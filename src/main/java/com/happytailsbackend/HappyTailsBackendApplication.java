4c0d8e7459241c1ab73d9ffa75b4036f4e1ee852
jenny-alexander <jenny-alexander@icloud.com>
2024-02-19 23:19:22 -0600

    Add spring boot project structure for controller, service and repository layers

---
+++ src/main/java/com/happytailsbackend/HappyTailsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.happytailsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTailsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyTailsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/happytailsbackend/HappyTailsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happytailsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyTailsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
